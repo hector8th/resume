257a0371cf825dea92eef17406a9f99d234c17c4
hector8th <115110070+hector8th@users.noreply.github.com>
2023-10-13 12:32:48 -0400

    updated resume

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Background from './components/Background.js';
 import Header from './components/Header.js';
 
+// cd C:\Users\iH3X\Desktop\CODING\GitHub\resume\resume-app
+
 class App extends Component {
     render() {
         return (
